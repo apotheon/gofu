a5416687e3fd9ea3fa906c2f98efe5794e05ed5f
apotheon <code@apotheon.net>
2017-03-01 23:49:17 +0000

    add ifstatement

---
+++ tour/ifstatement/ifstatement.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func pow(x, n, lim float64) {
+    if v += math.Pow(x, n); v < lim {
+        return v
+    }
+
+    return lim
+}
+
+func main() {
+    fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+}
