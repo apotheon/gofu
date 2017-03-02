315eff9eac08d317017735db0631debb6c095308
apotheon <code@apotheon.net>
2017-03-02 05:53:10 +0000

    add switch

---
+++ tour/switch/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func main() {
+    fmt.Print("Go runs on ")
+    switch os := runtime.GOOS; os {
+    case "darwin":
+        fmt.Println("McOS.")
+    case "linux":
+        fmt.Println("Loonix.")
+    default:
+        fmt.Println("%s.", os)
+    }
+}
